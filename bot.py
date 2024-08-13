f7d9de6d85021c1edc7b603fd8f1b49869f54f09
wsybok <wsybok@gmail.com>
2024-08-13 16:52:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -187,7 +187,7 @@ def webhook():
     else:
         abort(403)
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL + TOKEN)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+#if __name__ == "__main__":
+    #bot.remove_webhook()
+    #bot.set_webhook(url=WEBHOOK_URL + TOKEN)
+    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
